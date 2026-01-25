8e87391011715dafea700c669fe41079eedeb4fe
manjularkm90-max <manjularkm90@gmail.com>
2026-01-25 13:29:22 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,91 @@
+//1.Declare an integer variable and print it
+#include <stdio.h>
+int main() {
+    int a = 10;   
+    printf("%d", a);
+    return 0;
+}
+
+//2.Declare two integer variables and print them
+#include <stdio.h>
+int main() {
+    int a = 5, b = 15; 
+    printf("%d %d", a,b);
+    return 0;
+}
+
+//3.Declare a float variable and print it
+#include <stdio.h>
+int main() {
+    float x = 2.5;   
+    printf("%.2f",x);
+    return 0;
+}
+
+//4.Declare a character variable and print it
+#include <stdio.h>
+int main() {
+    char ch = 'X';   
+    printf("%c", ch);
+    return 0;
+}
+
+//5.Change variable value and print updated value
+#include <stdio.h>
+int main() {
+    int a = 10;
+    a = 20;   
+    printf("%d", a);
+    return 0;
+}
+
+//6.Add two variables
+#include <stdio.h>
+int main() {
+    int a = 5, b = 10;   
+    printf("%d", a+b);
+    return 0;
+}
+
+//7. Subtract two variable
+#include <stdio.h>
+int main() {
+    int a = 10, b = 5;   
+    printf("%d", a-b);
+    return 0;
+}
+
+//8.Multiply two variables
+#include <stdio.h>
+int main() {
+    int a = 4, b = 5;   
+    printf("%d", a*b);
+    return 0;
+}
+
+//9.divide two variables
+#include <stdio.h>
+int main() {
+    int a = 20, b = 5;   
+    printf("%d", a/b);
+    return 0;
+}
+
+//10.Assign value later and print
+#include <stdio.h>
+int main() {
+    int a;
+    a = 10;  
+    printf("%d",a);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+

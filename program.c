4c2b5d2f4b6a01980f93e51b04249ddf882f865a
manjularkm90-max <manjularkm90@gmail.com>
2026-01-25 13:28:31 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,73 @@
+//PRINTING STATEMENT
+//1.print hello world
+#include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
+
+//2.print your name
+
+#include <stdio.h>
+int main() {
+    printf("Sivan");
+    return 0;
+}
+
+//3.print an integer value
+
+#include <stdio.h>
+int main() {
+    printf("10");
+    return 0;
+}
+
+//4.print a floating point value
+
+#include <stdio.h>
+int main() {
+    printf("3.5");
+    return 0;
+}
+
+//5.print a character value
+
+#include <stdio.h>
+int main() {
+    printf("A");
+    return 0;
+}
+
+//6.print tw0 integer in one line
+
+#include <stdio.h>
+int main() {
+    printf("5 10");
+    return 0;
+}
+
+//7.print string along with number
+
+#include <stdio.h>
+int main() {
+    printf("Age: 20");
+    return 0;
+} 
+
+//8.print output in two line using \name
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello\nWorld");
+    return 0;
+}
+
+
+
+
+
+
+
+
